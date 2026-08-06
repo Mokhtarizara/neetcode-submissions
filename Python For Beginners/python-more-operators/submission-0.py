0e0b8554075729025cc08fb17e6cc48256045e0d
zahra <Gitzara@protonmail.com>
2026-08-06 13:49:14 -0700

    Add: python-more-operators - submission-0

---
+++ Python For Beginners/python-more-operators/submission-0.py
@@ -0,0 +1,17 @@
+a, b, c = 2, 8, 5
+
+
+# floor division = first / by second then round to int
+
+# modulus % return remainder first by second
+
+# **raised to first power of second operand
+
+
+product = a * b
+
+print(product // c)
+print(product % c)
+print(a ** b)
+print (b ** c)
+

aef57480284f060568924aba48373294cf3863a8
zahra <Gitzara@protonmail.com>
2026-08-06 12:22:57 -0700

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,11 @@
+
+variable = 2
+print(type(variable))
+variable = 2.4
+print(type(variable))
+variable = True
+print(type(variable))
+variable = "HI"
+print(type(variable))
+variable = [2,3]
+print(type(variable))

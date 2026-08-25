ca9ee877d37bf74bb6a5ff0e44fc93010c749df2
zahra <Gitzara@protonmail.com>
2026-08-24 23:24:36 -0700

    Add: python-dict-practice - submission-2

---
+++ Python For Beginners/python-dict-practice/submission-2.py
@@ -0,0 +1,21 @@
+from typing import Dict # this adds type hinting for Dict
+
+def count_characters(word: str) -> Dict[str, int]:
+    count = {}
+    # key = char 
+    # value = count of each char
+    
+    # the word each letter cheack on by one
+    for char in word: 
+        if char in count: #check if i have in jar 
+            count[char] += 1 # yes add 1 more
+        else:
+    # if no, new letter add new jar and add coin 
+            count[char] = 1 
+    return count # return the result 
+
+# don't modify below this line
+print(count_characters("hello"))
+print(count_characters("world"))
+print(count_characters("hello world"))
+print(count_characters("this is a longer sentence"))
